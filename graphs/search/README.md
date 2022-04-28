9fa3ccf85459e910a8691734b31d05a6f0f03a91
matthiebl <87024986+matthiebl@users.noreply.github.com>
2022-04-28 20:48:15 +1000

    Add initial files for graph search

---
+++ graphs/search/README.md
@@ -0,0 +1,43 @@
+# Graph Search - BFS - DFS
+
+## General Info
+
+This is a visual representation of the BFS and DFS algorithms.
+
+Try it out [here](https://matthiebl.github.io/visuals/graphs/search/)!
+
+Made with p5 library for JS
+
+## Credit
+
+This code is completely written by myself, however the algorithms used for
+BFS ([wikipedia](https://en.wikipedia.org/wiki/Breadth-first_search)) and
+DFS ([wikipedia](https://en.wikipedia.org/wiki/Depth-first_search))
+use the respective queue and stack implementations.
+
+
+
+## How to Set Up Locally
+
+If you want to set this up locally to play around, you need the the `search/`
+folder as well as the graph class `../Graph.js` and the p5js libararies
+folder `../../libraries/`.
+
+Either keep a similar directory tree as the repo:
+```
+project
+|
+├── libraries
+|   └── ...
+└── graphs
+    ├── search
+    |   ├── index.html
+    |   ├── bfs.js
+    |   ├── dfs.js
+    |   └── ...
+    └── Graph.js
+```
+
+or alter the relative path directory in `index.html` for the library files.
+
+Then opening the `index.html` file in a browser should display the canvas!
---
+++ graphs/search/dfs.js
@@ -0,0 +1,61 @@
+
+var dfs = function(p) {
+
+    p.setup = function(nV=50, seed=0) {
+        var cnv1 = p.createCanvas(0.375 * p.windowWidth, p.windowHeight);
+        cnv1.parent('dfs-sketch');
+
+        p.g = new Graph(nV);
+        
+        // Create some dummy vertices
+        let r = p.width / 30 * 3 / (nV ** 0.35);
+        p.vertices = [];
+        p.randomSeed(seed);
+        for (let i = 0; i < p.g.getNumVertices(); i++) {
+            let x = p.random(r + 5, p.width - (r + 5));
+            let y = p.random(r + 5, p.height - (r + 5));
+            while (p.vertices.some(v => p.dist(v.x, v.y, x, y) < 2 * r)) {
+                x = p.random(r + 5, p.width - (r + 5));
+                y = p.random(r + 5, p.height - (r + 5));
+            }
+
+            p.vertices.push({
+                x: x,
+                y: y,
+                r: r,
+            });
+        }
+        
+        p.allEdges = [];
+        for (let i = 0; i < p.g.getNumVertices(); i++) {
+            for (let j = i + 1; j < p.g.getNumVertices(); j++) {
+                p.g.addEdge(i, j, p.dist(p.vertices[i].x, p.vertices[i].y, p.vertices[j].x, p.vertices[j].y));
+                p.allEdges.push([i, j]);
+            }
+        }
+    }
+
+    p.step = function() {
+    }
+
+    p.draw = function() {
+        let rawSpeed = p.select('#speed').value();
+        if (rawSpeed === 0) {
+            p.frameRate(60);
+        } else {
+            let solveSpeed = p.map(rawSpeed, 0, 100, 0.75, 4) ** 3;
+            p.frameRate(solveSpeed);
+            p.step();
+        }
+
+        p.background(0);
+        
+        p.noFill();
+        p.stroke(255);
+        p.strokeWeight(10);
+        p.rect(0, 0, p.width, p.height);
+    }
+
+}
+
+var dfsSketch = new p5(dfs);
