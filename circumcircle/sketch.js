fb07888fa279c1dd6bcaba25e38b9946387a627b
matthiebl <87024986+matthiebl@users.noreply.github.com>
2022-04-30 16:58:56 +1000

    Update html and sketch to add bisectors

--- circumcircle/sketch.js
+++ circumcircle/sketch.js
@@ -43,22 +43,66 @@ function inCircumcircle(x, y) {
     return d <= r;
 }
 
+function isCounterClockwise() {
+    return (points[1].x - points[0].x)*(points[2].y - points[0].y)-(points[2].x - points[0].x)*(points[1].y - points[0].y) > 0;
+}
+
+function circumcircleContains(x, y) {
+    let ax_ = points[0].x - x;
+    let ay_ = points[0].y - y;
+    let bx_ = points[1].x - x;
+    let by_ = points[1].y - y;
+    let cx_ = points[2].x - x;
+    let cy_ = points[2].y - y;
+    let det = (
+        (ax_*ax_ + ay_*ay_) * (bx_*cy_-cx_*by_) -
+        (bx_*bx_ + by_*by_) * (ax_*cy_-cx_*ay_) +
+        (cx_*cx_ + cy_*cy_) * (ax_*by_-bx_*ay_)
+    )
+    if (isCounterClockwise()) {
+        return det > 0;
+    }
+    return det < 0;
+}
+
 function draw() {
     background(51);
+    
+    let a = points[0];
+    let b = points[1];
+    let c = points[2];
+    if (dist(centre.x, centre.y, a.x, a.y) > width/2) {
+        setup();
+    }
 
+    if (select('#detailed').checked()) {
+        stroke(255);
+        strokeWeight(1);
+        line(centre.x, centre.y, a.x + (b.x - a.x) / 2, a.y + (b.y - a.y) / 2);
+        line(centre.x, centre.y, a.x + (c.x - a.x) / 2, a.y + (c.y - a.y) / 2);
+        line(centre.x, centre.y, b.x + (c.x - b.x) / 2, b.y + (c.y - b.y) / 2);
+    }
+    
     // Draw edges of triangle
     stroke(0);
     strokeWeight(2);
-    line(points[0].x, points[0].y, points[1].x, points[1].y);
-    line(points[1].x, points[1].y, points[2].x, points[2].y);
-    line(points[2].x, points[2].y, points[0].x, points[0].y);
+    line(a.x, a.y, b.x, b.y);
+    line(b.x, b.y, c.x, c.y);
+    line(c.x, c.y, a.x, a.y);
 
     // Draw circumcircle
-    if (inCircumcircle(mouseX, mouseY)) {
+    if (circumcircleContains(mouseX, mouseY)) {
         fill(0, 255, 0, 50);
     } else {
         fill(255, 0, 0, 50);
     }
     strokeWeight(3);
-    circle(centre.x, centre.y, 2 * dist(centre.x, centre.y, points[0].x, points[0].y));
+    circle(centre.x, centre.y, 2 * dist(centre.x, centre.y, a.x, a.y));
+    
+    // Draw points
+    strokeWeight(10);
+    point(a.x, a.y);
+    point(b.x, b.y);
+    point(c.x, c.y);
+    point(centre.x, centre.y);
 }
